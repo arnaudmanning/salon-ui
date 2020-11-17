fa059ddd6934c147f4683f38dc41ae9055559418
astievet <arnaud.stievet@googlemail.com>
2020-11-17 15:14:16 +0100

    1.3. Setting up a React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+      <Router>
+        <div>
+          <nav className="navbar navbar-expand-lg bg-dark">
+            <a className="navbar-brand" href="/">AR Salon & Day Spa</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                {<li className="nav-item active">
+                  <a className="nav-link" href="/">Services <span className="sr-only">(current)</span></a>
+                </li>}
+              </ul>
+            </div>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+  );
+
+  function Home() {
+    return <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
-  );
+  }
 }
-
-export default App;
